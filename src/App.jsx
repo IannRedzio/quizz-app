121429bc5c4db116bc29d81f357ab7bbe7b043a3
IannRedzio <ianredzio13@gmail.com>
2021-09-27 13:03:28 -0300

    refactor data

--- src/App.jsx
+++ src/App.jsx
@@ -1,83 +1,15 @@
-import { useState, useEffect, useMemo } from "react";
 import "./app.css";
+import { useEffect, useMemo, useState } from "react";
+import data from "./common/data";
+import Start from "./components/Start";
 import Timer from "./components/Timer";
 import Trivia from "./components/Trivia";
-import Start from "./components/Start";
 
 function App() {
   const [username, setUsername] = useState(null);
+  const [timeOut, setTimeOut] = useState(false);
   const [questionNumber, setQuestionNumber] = useState(1);
-  const [stop, setStop] = useState(false);
   const [earned, setEarned] = useState("$ 0");
-
-  const data = [
-    {
-      id: 1,
-      question: "Rolex is a company that specializes in what type of product?",
-      answers: [
-        {
-          text: "Phone",
-          correct: false,
-        },
-        {
-          text: "Watches",
-          correct: true,
-        },
-        {
-          text: "Food",
-          correct: false,
-        },
-        {
-          text: "Cosmetic",
-          correct: false,
-        },
-      ],
-    },
-    {
-      id: 2,
-      question: "When did the website `Facebook` launch?",
-      answers: [
-        {
-          text: "2004",
-          correct: true,
-        },
-        {
-          text: "2005",
-          correct: false,
-        },
-        {
-          text: "2006",
-          correct: false,
-        },
-        {
-          text: "2007",
-          correct: false,
-        },
-      ],
-    },
-    {
-      id: 3,
-      question: "Who played the character of harry potter in movie?",
-      answers: [
-        {
-          text: "Johnny Deep",
-          correct: false,
-        },
-        {
-          text: "Leonardo Di Caprio",
-          correct: false,
-        },
-        {
-          text: "Denzel Washington",
-          correct: false,
-        },
-        {
-          text: "Daniel Red Cliff",
-          correct: true,
-        },
-      ],
-    },
-  ];
 
   const moneyPyramid = useMemo(
     () =>
@@ -104,28 +36,33 @@ function App() {
   useEffect(() => {
     questionNumber > 1 &&
       setEarned(moneyPyramid.find((m) => m.id === questionNumber - 1).amount);
-  }, [moneyPyramid, questionNumber]);
+  }, [questionNumber, moneyPyramid]);
 
   return (
     <div className="app">
-      {username ? (
+      {!username ? (
+        <Start setUsername={setUsername} />
+      ) : (
         <>
           <div className="main">
-            {stop ? (
+            {timeOut ? (
               <h1 className="endText">You earned: {earned}</h1>
             ) : (
               <>
                 <div className="top">
                   <div className="timer">
-                    <Timer setStop={setStop} questionNumber={questionNumber} />
+                    <Timer
+                      setTimeOut={setTimeOut}
+                      questionNumber={questionNumber}
+                    />
                   </div>
                 </div>
                 <div className="bottom">
                   <Trivia
                     data={data}
-                    setStop={setStop}
                     questionNumber={questionNumber}
                     setQuestionNumber={setQuestionNumber}
+                    setTimeOut={setTimeOut}
                   />
                 </div>
               </>
@@ -148,8 +85,6 @@ function App() {
             </ul>
           </div>
         </>
-      ) : (
-        <Start setUsername={setUsername} />
       )}
     </div>
   );
