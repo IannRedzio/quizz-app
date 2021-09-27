66f4068719e8d1b1ec5326a7d94c93d403be2631
IannRedzio <ianredzio13@gmail.com>
2021-09-27 13:55:27 -0300

    add 15 questions

--- src/common/data.js
+++ src/common/data.js
@@ -1,22 +1,22 @@
 const data = [
   {
     id: 1,
-    question: "Rolex is a company that specializes in what type of product?",
+    question: "Rolex es una compania que se especializa en que producto?",
     answers: [
       {
-        text: "Phone",
+        text: "Celulares",
         correct: false,
       },
       {
-        text: "Watches",
+        text: "Relojes",
         correct: true,
       },
       {
-        text: "Food",
+        text: "Comida",
         correct: false,
       },
       {
-        text: "Cosmetic",
+        text: "Cosmetica",
         correct: false,
       },
     ],
@@ -62,6 +62,270 @@ const data = [
       {
         text: "Daniel Red Cliff",
         correct: true,
+      },
+    ],
+  },
+  {
+    id: 4,
+    question: "¿De qué murió el compositor Chopin?",
+    answers: [
+      {
+        text: "De un ataque al corazón",
+        correct: false,
+      },
+      {
+        text: "De tuberculosis",
+        correct: true,
+      },
+      {
+        text: "De insuficiencia renal aguda",
+        correct: false,
+      },
+      {
+        text: "De un cáncer estomacal",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 5,
+    question: "En la tabla periódica de los elementos, ¿hay cuatro elementos diferentes cuyos nombres se basan en…?",
+    answers: [
+      {
+        text: "En el gato mascota de Antoine Lavoisier",
+        correct: false,
+      },
+      {
+        text: "En la luna de Neptuno",
+        correct: false,
+      },
+      {
+        text: "En el nombre de soltera de Marie Curie",
+        correct: false,
+      },
+      {
+        text: "En una aldea minera sueca",
+        correct: true,
+      },
+    ],
+  },
+  {
+    id: 6,
+    question: "¿Qué famosa estrella musical fue conocida como Baby Frances Gumm en la fase inicial de su carrera?",
+    answers: [
+      {
+        text: "Ginger Rogers",
+        correct: false,
+      },
+      {
+        text: "Deanna Durbin",
+        correct: false,
+      },
+      {
+        text: "Doris Day",
+        correct: false,
+      },
+      {
+        text: "Judy Garland",
+        correct: true,
+      },
+    ],
+  },
+  {
+    id: 7,
+    question: "¿En la corte de qué rey pasó Leonardo Da Vinci los dos últimos años de su vida?",
+    answers: [
+      {
+        text: "Enrique IV",
+        correct: false,
+      },
+      {
+        text: "Carlos III",
+        correct: false,
+      },
+      {
+        text: "Luis XII",
+        correct: false,
+      },
+      {
+        text: "Francisco I",
+        correct: true,
+      },
+    ],
+  },
+  {
+    id: 8,
+    question: "La gente que tiene una reacción física importante hacia el arte hermoso, ¿qué tipo de síndrome sufre?",
+    answers: [
+      {
+        text: "Síndrome de Proust",
+        correct: false,
+      },
+      {
+        text: "Síndrome de Jerusalén",
+        correct: false,
+      },
+      {
+        text: "Síndrome de Stendhal",
+        correct: true,
+      },
+      {
+        text: "Síndrome de Beckett",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 9,
+    question: "¿Cuál de los siguientes hombres no tiene un elemento químico llamado como él?",
+    answers: [
+      {
+        text: "Albert Einstein",
+        correct: false,
+      },
+      {
+        text: "Niels Bohr",
+        correct: false,
+      },
+      {
+        text: "Isaac Newton",
+        correct: true,
+      },
+      {
+        text: "Enrico Fermi",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 10,
+    question: "¿Quién fue el primer hombre en viajar dos veces al espacio?",
+    answers: [
+      {
+        text: "Vladimir Titov",
+        correct: false,
+      },
+      {
+        text: "Michael Collins",
+        correct: false,
+      },
+      {
+        text: "Gus Grissom",
+        correct: true,
+      },
+      {
+        text: "Yuri Gagarin",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 11,
+    question: "¿En que año mataron a John Lennon?",
+    answers: [
+      {
+        text: "1980",
+        correct: true,
+      },
+      {
+        text: "1985",
+        correct: false,
+      },
+      {
+        text: "1990",
+        correct: false,
+      },
+      {
+        text: "1982",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 12,
+    question: "¿Cuántos mares rodean la península de los Balcanes?",
+    answers: [
+      {
+        text: "3",
+        correct: false,
+      },
+      {
+        text: "4",
+        correct: false,
+      },
+      {
+        text: "5",
+        correct: false,
+      },
+      {
+        text: "6",
+        correct: true,
+      },
+    ],
+  },
+  {
+    id: 13,
+    question: "¿Cuál de las siguientes razas caninas tiene ejemplares que alcanzan más peso con respecto a las démas?",
+    answers: [
+      {
+        text: "Terranova",
+        correct: true,
+      },
+      {
+        text: "Bull terrier",
+        correct: false,
+      },
+      {
+        text: "Shiba inu",
+        correct: false,
+      },
+      {
+        text: "Samoyedo",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 14,
+    question: "¿Cuál es el gentilicio correspondiente a quien nació en Lesoto, Africa?",
+    answers: [
+      {
+        text: "Lesoteño",
+        correct: false,
+      },
+      {
+        text: "Lesotonense",
+        correct: false,
+      },
+      {
+        text: "Lesotino",
+        correct: false,
+      },
+      {
+        text: "Lesotense",
+        correct: true,
+      },
+    ],
+  },
+  {
+    id: 15,
+    question: "¿Cuantos huesos tiene un humano adulto?",
+    answers: [
+      {
+        text: "300",
+        correct: false,
+      },
+      {
+        text: "220",
+        correct: false,
+      },
+      {
+        text: "206",
+        correct: true,
+      },
+      {
+        text: "306",
+        correct: false,
       },
     ],
   },
